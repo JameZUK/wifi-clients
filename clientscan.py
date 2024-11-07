b899a9b841e0724f5ea5addc2af7c90e444d6c5b
James <spam@jmuk.net>
2024-11-07 08:45:33 +0000

    Update clientscan.py

--- clientscan.py
+++ clientscan.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import threading
 import signal
 import sys
+import argparse
 
 # Dictionary to store SSID -> set of client MAC addresses
 network_clients = defaultdict(set)
@@ -18,7 +19,7 @@ def packet_handler(packet):
         ssid = packet[Dot11Elt].info.decode('utf-8', errors='ignore')
         bssid = packet[Dot11].addr2
         # Signal strength
-        signal = packet.dBm_AntSignal
+        signal = packet.dBm_AntSignal if hasattr(packet, 'dBm_AntSignal') else 'N/A'
         network_signals[ssid] = signal
 
     # Check for Data frames to find clients associated with APs
@@ -33,10 +34,8 @@ def packet_handler(packet):
                 # Assuming addr1 is the AP and addr2 is the client
                 # Or vice versa, depending on the ToDS and FromDS bits
                 # For simplicity, we consider both possibilities
-                for ssid, signal in network_signals.items():
-                    network_clients[ssid].add(addr1)
-                    network_clients[ssid].add(addr2)
-                    network_clients[ssid].add(addr3)
+                for ssid in network_signals.keys():
+                    network_clients[ssid].update([addr1, addr2, addr3])
 
 # Function to stop sniffing on signal interrupt
 def stop_sniff(signal, frame):
@@ -47,17 +46,23 @@ def stop_sniff(signal, frame):
 signal.signal(signal.SIGINT, stop_sniff)
 
 def main():
-    print("Starting WiFi scan. Press Ctrl+C to stop.")
+    parser = argparse.ArgumentParser(description="WiFi Scanner to count clients per SSID.")
+    parser.add_argument('-i', '--interface', required=True, help='Wireless interface in monitor mode (e.g., wlan0mon)')
+    args = parser.parse_args()
+
+    interface = args.interface
+
+    print(f"Starting WiFi scan on interface {interface}. Press Ctrl+C to stop.")
 
     # Start sniffing packets
-    sniff(iface="wlan0", prn=packet_handler, store=0)
+    sniff(iface=interface, prn=packet_handler, store=0)
 
     # After sniffing is stopped, display the results
     print("{:<30} {:<15} {:<10}".format('SSID', 'Clients', 'Signal(dBm)'))
     print("-" * 60)
     for ssid, clients in network_clients.items():
-        signal = network_signals.get(ssid, 'N/A')
-        print("{:<30} {:<15} {:<10}".format(ssid, len(clients), signal))
+        signal_strength = network_signals.get(ssid, 'N/A')
+        print("{:<30} {:<15} {:<10}".format(ssid, len(clients), signal_strength))
 
 if __name__ == "__main__":
     main()
