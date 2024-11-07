751c043afb85d3b6373ab3fa3d41e06f3039741e
James <spam@jmuk.net>
2024-11-07 09:03:00 +0000

    Update clientscan.py

--- clientscan.py
+++ clientscan.py
@@ -17,49 +17,58 @@ bssid_signal_strength = {}
 
 # Global variable to control sniffing
 sniffing = True
+# Global variable to control debug output
+debug_mode = False
 
 # Function to handle packet processing
 def packet_handler(packet):
+    # Debugging output to confirm packet capture
+    if debug_mode:
+        print(f"Captured packet: {packet.summary()}")
+
     # Check for Beacon and Probe Response frames (to get SSIDs)
     if packet.haslayer(Dot11Beacon) or packet.haslayer(Dot11ProbeResp):
         ssid = packet[Dot11Elt].info.decode('utf-8', errors='ignore')
         bssid = packet[Dot11].addr2
         # Signal strength
         signal = packet.dBm_AntSignal if hasattr(packet, 'dBm_AntSignal') else 'N/A'
+        if debug_mode:
+            print(f"Detected SSID: {ssid}, BSSID: {bssid}, Signal: {signal}")
         bssid_ssid_map[bssid] = ssid
         bssid_signal_strength[bssid] = signal
 
     # Check for Data frames to find clients associated with APs
-    if packet.haslayer(Dot11):
-        if packet.type == 2 and packet.subtype == 0:  # Data frames
-            addr1 = packet.addr1  # Receiver MAC
-            addr2 = packet.addr2  # Transmitter MAC
-            addr3 = packet.addr3  # BSSID
+    if packet.haslayer(Dot11) and packet.type == 2 and packet.subtype == 0:  # Data frames
+        addr1 = packet.addr1  # Receiver MAC
+        addr2 = packet.addr2  # Transmitter MAC
+        addr3 = packet.addr3  # BSSID
 
-            # ToDS and FromDS bits
-            to_ds = packet.FCfield & 0x1 != 0
-            from_ds = packet.FCfield & 0x2 != 0
+        # ToDS and FromDS bits
+        to_ds = packet.FCfield & 0x1 != 0
+        from_ds = packet.FCfield & 0x2 != 0
 
-            if to_ds and not from_ds:
-                # Data frame from station to AP
-                client_mac = addr2
-                bssid = addr1
-            elif from_ds and not to_ds:
-                # Data frame from AP to station
-                client_mac = addr1
-                bssid = addr2
-            elif not from_ds and not to_ds:
-                # Data frame between stations in the same BSS
-                client_mac = addr2
-                bssid = addr3
-            else:
-                # WDS frame or other
-                return
+        if to_ds and not from_ds:
+            # Data frame from station to AP
+            client_mac = addr2
+            bssid = addr1
+        elif from_ds and not to_ds:
+            # Data frame from AP to station
+            client_mac = addr1
+            bssid = addr2
+        elif not from_ds and not to_ds:
+            # Data frame between stations in the same BSS
+            client_mac = addr2
+            bssid = addr3
+        else:
+            # WDS frame or other
+            return
 
-            # Map client to SSID using BSSID
-            ssid = bssid_ssid_map.get(bssid)
-            if ssid:
-                network_clients[ssid].add(client_mac)
+        # Map client to SSID using BSSID
+        ssid = bssid_ssid_map.get(bssid)
+        if ssid:
+            if debug_mode:
+                print(f"Associating client {client_mac} with SSID {ssid}")
+            network_clients[ssid].add(client_mac)
 
 # Function to stop sniffing
 def stop_sniffing(signum, frame):
@@ -82,7 +91,11 @@ def main():
     parser = argparse.ArgumentParser(description="WiFi Scanner to count clients per SSID.")
     parser.add_argument('-i', '--interface', required=True, help='Wireless interface in monitor mode (e.g., wlan0mon)')
     parser.add_argument('-t', '--interval', type=int, default=5, help='Interval in seconds between output updates')
+    parser.add_argument('--debug', action='store_true', help='Enable debug output')
     args = parser.parse_args()
+
+    global debug_mode
+    debug_mode = args.debug
 
     interface = args.interface
     interval = args.interval
